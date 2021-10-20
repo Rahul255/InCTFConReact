270b475092b39a1667179296ef6bf07d5619c647
Rahul255 <rahulmohan255@gmail.com>
2021-10-20 09:33:06 +0530

    file added

--- src/Components/Sponsor/Sponsor.js
+++ src/Components/Sponsor/Sponsor.js
@@ -1,33 +1,97 @@
-import React from 'react'
-import './Sponsor.css'
-import brand1 from '../../images/sponsor/salesforce.png'
-import brand2 from '../../images/sponsor/zoho-1e78182f2870ef5f7277b10047c556e1.png'
-import brand3 from '../../images/sponsor/vmware-54e2f942b7201762830671db2d5b73ae.png'
-import brand4 from '../../images/sponsor/crowdstrike.png'
-import brand5 from '../../images/sponsor/social-cred.png'
-import brand6 from '../../images/sponsor/hrblock.jpeg'
+import React from "react";
+import "./Sponsor.css";
+import brand1 from "../../images/sponsor/salesforce.png";
+import brand2 from "../../images/sponsor/zoho-1e78182f2870ef5f7277b10047c556e1.png";
+import brand3 from "../../images/sponsor/vmware-54e2f942b7201762830671db2d5b73ae.png";
+import brand4 from "../../images/sponsor/crowdstrike.png";
+import brand5 from "../../images/sponsor/social-cred.png";
+import brand6 from "../../images/sponsor/hrblock.jpeg";
 
 function Sponsor() {
-    return (
-        <div className="sponsor">
-            <div class="container">
-                <div class="section_title">
-                    <h3 class="title">
-                        Our Sponsor
-                    </h3>
-                </div>
-                <div class="row">
+  var data = [
+    {
+      image: require("../../images/sponsor/salesforce.png").default,
+      description: "Event Sponsor",
+      maxHeight: "220px",
+    },
+    {
+      image:
+        require("../../images/sponsor/zoho-1e78182f2870ef5f7277b10047c556e1.png")
+          .default,
+      description: "Diamond Sponsor",
+      maxHeight: "220px",
+    },
+    {
+      image:
+        require("../../images/sponsor/vmware-54e2f942b7201762830671db2d5b73ae.png")
+          .default,
+      description: "Diamond Sponsor",
+      maxHeight: "220px",
+    },
+    {
+      image: require("../../images/sponsor/crowdstrike.png").default,
+      description: "Platinum Sponsor",
+      maxHeight: "180px",
+    },
+    {
+      image: require("../../images/sponsor/social-cred.png").default,
+      description: "Gold sponsor",
+      maxHeight: "220px",
+    },
+    {
+      image: require("../../images/sponsor/hrblock.jpeg").default,
+      description: "Silver Sponsor",
+      maxHeight: "180px",
+    },
+  ];
 
-                    {/* <div class="col-lg-5 event-header brand-m-p">
+  return (
+    <div className="sponsor">
+      <div class="container">
+        <div class="section_title">
+          <h3 class="title">Our Sponsor</h3>
+        </div>
+        <div className="row">
+          <p
+            className=" col-12 text-center opacity-8 mb-3 font-weight-bold"
+            style={{
+              maxWidth: "100%",
+              fontSize: "95%",
+              lineHeight: 1.4,
+              width: "450px",
+            }}
+          >
+            We thank these organizations for investing in the CyberSecurity
+            Workforce of Tomorrow.
+          </p>
+        </div>
+        <div class="row">
+          {/* <div class="col-lg-5 event-header brand-m-p">
 
                         <h3>Brands and companies trust us<b>.</b> </h3>
                         <p>Here is the beginning of a great season. Thanks again for your generous sponsorship. We hope to live up to
                             our achievements of previous years. We promise to keep you updated on our progress.</p>
 
                     </div> */}
-                    {/* <div class="col-lg-2"></div> */}
-                    <div class="col-12 col-lg-12 col-md-12 m-md-auto">
-                        <div class="row brand_1">
+          {/* <div class="col-lg-2"></div> */}
+          {data.map((s, index) => (
+            <div
+              className="col-12 col-lg-4 col-md-4 m-md-auto justify-center text-center items-end"
+              key={index}
+            >
+              <div className="text-center">
+                <img
+                  style={{ maxHeight: s.maxHeight }}
+                  draggable="false"
+                  src={s.image}
+                  alt="brand-img"
+                  class="img-fluid p-2"
+                />
+                {s.description && (<div className="font-weight-bold mt-3 font-light">{s.description}</div>)}
+              </div>
+            </div>
+          ))}
+          {/* <div class="row brand_1">
                             <div class="col-lg-3 col-md-3 col-3 text-center">
                                 <img draggable="false" src={brand1} alt="brand-img" class="img-fluid" />
                                 <h5>Gold Sponsor</h5>
@@ -43,8 +107,8 @@ function Sponsor() {
                                 <h5>Gold Sponsor</h5>
                             </div>
                         </div>
-                        <br /><br />
-                        <div class="row brand_2">
+                        <br /><br /> */}
+          {/* <div class="row brand_2">
                             <div class="col-lg-3 col-3 col-md-3 text-center">
                                 <img draggable="false" src={brand4} alt="brand-img" class="img-fluid" />
                                 <h5>Gold Sponsor</h5>
@@ -59,12 +123,11 @@ function Sponsor() {
                                 <img draggable="false" src={brand6} alt="brand-img" class="img-fluid" />
                                 <h5>Gold Sponsor</h5>
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                        </div> */}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
-export default Sponsor
+export default Sponsor;
