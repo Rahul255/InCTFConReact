3b2debff588349827401ef14da5e12a460bc88f5
Rahul M <rahulmohan255@gmail.com>
2021-12-06 10:36:39 +0530

    file added

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -10,27 +10,27 @@ const CoverContainer = styled.section`
     position: relative;
     overflow: hidden;
 `;
-const GoalCard = styled.div`
-    img {
-        text-align: center;
-        display: block;
-        max-width: 50%;
-    }
-    p {
-      color: #AAA;
-      margin-left: 19px;
-      margin-top: 13px;
-      font-size: 10px;
-    }
-    transition: all 1s ease;
-    // &:hover {
-    //     transform: translateY(10px);
-    //     transition: all 1s ease;
-    //     p {
-    //        color: white;
-    //     }
-    // }
-`;
+// const GoalCard = styled.div`
+//     img {
+//         text-align: center;
+//         display: block;
+//         max-width: 50%;
+//     }
+//     p {
+//       color: #AAA;
+//       margin-left: 19px;
+//       margin-top: 13px;
+//       font-size: 10px;
+//     }
+//     transition: all 1s ease;
+//     // &:hover {
+//     //     transform: translateY(10px);
+//     //     transition: all 1s ease;
+//     //     p {
+//     //        color: white;
+//     //     }
+//     // }
+// `;
 const CoverWrap = styled.div`
     {/*backdrop-filter: blur(1px) brightness(0.45);*/}
     width: 100%;
@@ -83,33 +83,33 @@ const CoverWrap = styled.div`
         margin-right: 5px;
     }
 `
-const inCTFSponsors = [
-    {
-        image: require("../../images/sponsor/salesforce.png").default,
-        text: "EVENT SPONSOR",
-    },
-    {
-        image: require("../../images/sponsor/zoho-1e78182f2870ef5f7277b10047c556e1.png").default,
-        text: "DIAMOND SPONSOR",
+// const inCTFSponsors = [
+//     {
+//         image: require("../../images/sponsor/salesforce.png").default,
+//         text: "EVENT SPONSOR",
+//     },
+//     {
+//         image: require("../../images/sponsor/zoho-1e78182f2870ef5f7277b10047c556e1.png").default,
+//         text: "DIAMOND SPONSOR",
         
-    },
-    {
-        image: require("../../images/sponsor/vmware-54e2f942b7201762830671db2d5b73ae.png").default,
-        text: "DIAMOND SPONSOR"
-    },
-    // {
-    //     image: require("../../images/sponsor/crowdstrike.png").default,
-    //     text: "PLATINUM SPONSOR",
-    // },
-    // {
-    //     image: require("../../images/sponsor/social-cred.png").default,
-    //     text: "GOLD SPONSOR"
-    // },
-    // {
-    //     image: require("../../images/sponsor/hrblock.jpeg").default,
-    //     text: "GOLD SPONSOR"
-    // },
-];
+//     },
+//     {
+//         image: require("../../images/sponsor/vmware-54e2f942b7201762830671db2d5b73ae.png").default,
+//         text: "DIAMOND SPONSOR"
+//     },
+//     // {
+//     //     image: require("../../images/sponsor/crowdstrike.png").default,
+//     //     text: "PLATINUM SPONSOR",
+//     // },
+//     // {
+//     //     image: require("../../images/sponsor/social-cred.png").default,
+//     //     text: "GOLD SPONSOR"
+//     // },
+//     // {
+//     //     image: require("../../images/sponsor/hrblock.jpeg").default,
+//     //     text: "GOLD SPONSOR"
+//     // },
+// ];
 
 function Banner() {
     return (
@@ -148,21 +148,21 @@ function Banner() {
                     </div>
                    
                 </CoverWrap>
-                <div className="container" style={{ maxWidth: 'auto', backgroundColor: "white" }}>
+                {/* <div className="container" style={{ maxWidth: 'auto', backgroundColor: "white" }}>
                             <div className="d-flex" style={{backgroundColor: "white"}}>
                                 {inCTFSponsors.map((g, index) =>
                                 
                                     <GoalCard className="col-4 col-md-4 col-lg-4 p-2" style={{marginLeft:""}}>
-                                        {/* <Fade up={index % 2 === 0} down={index % 2 !== 0} delay={index * 350}> */}
+                                        <Fade up={index % 2 === 0} down={index % 2 !== 0} delay={index * 350}>
                                             <img alt="illustration" draggable="false" src={g.image} />
                                             <p>{g.text}</p>
-                                        {/* </Fade> */}
+                                        </Fade>
                                         
                                     </GoalCard>
                                     
                                 )}
                             </div>
-                    </div>
+                    </div> */}
             </CoverContainer>
         </React.Fragment>
     )
