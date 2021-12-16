4007f06f774917f7329a3ffc6d5effe94c6465ad
Rahul M <rahulmohan255@gmail.com>
2021-12-16 12:02:33 +0530

    file added

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
         <Route path='/schedule'>
           <Schedules />
         </Route>
-        <Route path='/guest_speaker'>
+        <Route path='/speakers'>
           <Guest />
         </Route>
         <Route path='/promote'>
