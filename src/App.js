c33bde125b80f9f5c3a87ba83c51348f5a8a7f10
Rahul M <rahulmohan255@gmail.com>
2022-01-09 11:22:51 +0530

    file added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './Pages/Home';
-import About from './Pages/AboutUs';
-import Speakers from './Pages/Speakers';
-import InCTF2019 from './Pages/InCTF2019';
+// import About from './Pages/AboutUs';
+// import Speakers from './Pages/Speakers';
+// import InCTF2019 from './Pages/InCTF2019';
 import Schedules from './Pages/Schedules';
 import Guest from './Pages/Guest';
 import Promote from './Pages/Promote';
@@ -23,15 +23,15 @@ function App() {
         <Route exact path='/'>
           <Home />
         </Route>
-        <Route path='/about'>
+        {/* <Route path='/about'>
           <About />
         </Route>
         <Route path='/trainings'>
           <Speakers />
-        </Route>
-        <Route path='/inctf2019'>
+        </Route> */}
+        {/* <Route path='/inctf2019'>
           <InCTF2019 />
-        </Route>
+        </Route> */}
         <Route path='/schedule'>
           <Schedules />
         </Route>
